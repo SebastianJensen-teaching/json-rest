83a7b4079128e4c0735b6b3b11a04c76d874024c
Sebastian Jensen <sebastian.jensen@ga.ntig.se>
2021-03-11 12:39:01 +0100

    first commit

---
+++ app-async.js
@@ -0,0 +1,23 @@
+async function getMovies(path) {
+  const response = await fetch(path);
+  const data = await response.json();
+  data.forEach((movie) => {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("movie-item");
+    let movieTitle = document.createElement("h2");
+    movieTitle.innerText = movie.title;
+    newDiv.append(movieTitle);
+    let movieDirector = document.createElement("h3");
+    movieDirector.innerText = "Director: " + movie.director;
+    newDiv.append(movieDirector);
+    let rating = document.createElement("p");
+    let numRating = parseInt(movie.rating);
+    for (let i = 0; i < numRating; i++) {
+      rating.innerText += "⭐";
+    }
+    newDiv.append(rating);
+    document.querySelector("#app-root").append(newDiv);
+  });
+}
+
+getMovies("./movies.json");
---
+++ app.js
@@ -0,0 +1,28 @@
+fetch("./movies.json")
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    data.forEach((movie) => {
+      let newDiv = document.createElement("div");
+      newDiv.classList.add("movie-item");
+      let movieTitle = document.createElement("h2");
+      movieTitle.innerText = movie.title;
+      newDiv.append(movieTitle);
+      let movieDirector = document.createElement("h3");
+      movieDirector.innerText = "Director: " + movie.director;
+      newDiv.append(movieDirector);
+      let rating = document.createElement("p");
+      let numRating = parseInt(movie.rating);
+      for (let i = 0; i < numRating; i++) {
+        rating.innerText += "⭐";
+      }
+      newDiv.append(rating);
+      document.querySelector("#app-root").append(newDiv);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
